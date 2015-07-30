5749bed2ca950917427989f6c81f332067a4fb16
Louis Roy <louis.roy@cossette.com>
2015-07-30 10:33:07 -0400

    tests

--- test/index.js
+++ test/index.js
@@ -3,4 +3,31 @@ require('chai').should();
 var dotenv = require('dotenv');
 	dotenv.load();
 
-var trader = require('../index.js');
+var _ = require('underscore');
+var inflect = require('inflect');
+
+var trader = require('../index.js')(process.env.AUTH_TOKEN);
+
+var resources = ['advertiser', 'campaign', 'creative', 'flight'];
+
+_.each(resources, function(resource, index, list) {
+	var method = 'get' + inflect.capitalize(inflect.pluralize(resource));
+	
+	describe('#' + method, function() {
+		it('gets all ' + inflect.pluralize(resource), function() {
+			(function() {
+				trader[method]();
+			}).should.throw(Error);
+			
+			(function() {
+				trader[method](null);
+			}).should.throw(Error);
+			
+			(function() {
+				trader[method](null, null);
+			}).should.throw(Error);
+			
+			trader[method](null, function() {}).should.be.ok();
+		});
+	});
+});
