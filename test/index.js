84fe80462d47c0510392f2f16bd08f48c9e43926
Louis Roy <louis.roy@cossette.com>
2015-07-30 10:47:49 -0400

    tests

--- test/index.js
+++ test/index.js
@@ -8,26 +8,68 @@ var inflect = require('inflect');
 
 var trader = require('../index.js')(process.env.AUTH_TOKEN);
 
-var resources = ['advertiser', 'campaign', 'creative', 'flight'];
+var resources = ['advertiser' , 'campaign', 'creative', 'flight'];
 
 _.each(resources, function(resource, index, list) {
-	var method = 'get' + inflect.capitalize(inflect.pluralize(resource));
+	var getAll = 'get' + inflect.capitalize(inflect.pluralize(resource));
+	var getSingle = 'get' + inflect.capitalize(resource);
+	var patchSingle = 'patch' + inflect.capitalize(resource);
 	
-	describe('#' + method, function() {
+	describe('#' + getAll, function() {
 		it('gets all ' + inflect.pluralize(resource), function() {
 			(function() {
-				trader[method]();
+				trader[getAll]();
 			}).should.throw(Error);
 			
 			(function() {
-				trader[method](null);
+				trader[getAll](null);
 			}).should.throw(Error);
 			
 			(function() {
-				trader[method](null, null);
+				trader[getAll](null, null);
 			}).should.throw(Error);
 			
-			trader[method](null, function() {}).should.be.ok();
+			trader[getAll](null, function() {}).should.be.ok;
 		});
+		
+		describe('#' + getSingle, function() {
+			it('gets a ' + inflect.pluralize(resource), function() {
+				(function() { trader[getSingle](); }).should.throw(Error);
+				
+				(function() { trader[getSingle](null); }).should.throw(Error);
+				
+				(function() { trader[getSingle](null, null); }).should.throw(Error);
+				
+				(function() { trader[getSingle](null, null, null); }).should.throw(Error);
+				
+				(function() { trader[getSingle](4014, null, null); }).should.throw(Error);
+				
+				(function() { trader[getSingle](null, null, function() {}); }).should.throw(Error);
+				
+				trader[getSingle](4014, {}, function() {}).should.be.ok;
+				trader[getSingle](4014, null, function() {}).should.be.ok;
+			});
+		});
+		/*
+		describe('#' + patchSingle, function() {
+			it('patches a ' + inflect.pluralize(resource), function() {
+				(function() { trader[patchSingle](); }).should.throw(Error);
+				
+				(function() { trader[patchSingle](null); }).should.throw(Error);
+				
+				(function() { trader[patchSingle](null, null); }).should.throw(Error);
+				
+				(function() { trader[patchSingle](null, null, null); }).should.throw(Error);
+				
+				(function() { trader[patchSingle](4014, null, null); }).should.throw(Error);
+				
+				(function() { trader[patchSingle](null, null, function() {}); }).should.throw(Error);
+				
+				(function() { trader[patchSingle](4014, null, function() {}); }).should.throw(Error);
+				
+				trader[patchSingle](4014, { name:"Test " + Math.random() }, function() {  }).should.be.ok;
+			});
+		});
+		*/
 	});
 });
